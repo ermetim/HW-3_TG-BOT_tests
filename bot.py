6d1f16fbeb329a772c32d56de8c169371b8ccabb
ermetim <ermetim@gmail.com>
2024-02-21 06:02:46 +0300

    Add buttons, add dotenv

--- bot.py
+++ bot.py
@@ -5,15 +5,16 @@ import pickle
 import cv2
 import numpy as np
 
-from aiogram import Bot, Dispatcher, types, F
+from dotenv import load_dotenv, dotenv_values
+
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.filters.callback_data import CallbackData
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import Message
-
-
+from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from functions import ImageProcessing
 
 MODEL_NAMES = ['lgbm_model_hog_best.pkl',
@@ -21,9 +22,12 @@ MODEL_NAMES = ['lgbm_model_hog_best.pkl',
                'rfc_model_hog_best.pkl',
                'rfc_model_pic_best.pkl',
                ]
+load_dotenv()
+config = dotenv_values(".env")
+BOT_TOKEN = config['BOT_TOKEN']
 
 # fake token
-BOT_TOKEN = '123456789:AABBCCDDEEFFaabbccddeeff-1234567890'
+# BOT_TOKEN = '123456789:AABBCCDDEEFFaabbccddeeff-1234567890'
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -40,67 +44,112 @@ class TestCallbackData(CallbackData, prefix="test_callback_data"):
 dp = Dispatcher(storage=MemoryStorage())
 
 
+def generate_keyboard(button_names, n=2):
+    """
+    Создание списка кнопок по строкам и возвращение объекта keyboard
+    :param button_names: список кнопок
+    :param n: Количество кнопок в ряду
+    :return: список кнопок типа KeyboardButton(text="button_name")
+    """
+    if type(button_names) is dict:
+        button_names = list(button_names.keys())
+    keyboard_buttons = []
+    for i in range(0, len(button_names), n):
+        lines = []
+        for button in button_names[i:i+n]:
+            lines.append(KeyboardButton(text=button))
+        keyboard_buttons.append(lines)
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=keyboard_buttons,
+        resize_keyboard=True,
+        input_field_placeholder="'Выберите ответ'",
+        one_time_keyboard=True
+    )
+    return keyboard
+
+
 class States(StatesGroup):
-    state = State()
-    state_1 = State()
-    state_2 = State()
+    yes_no = State()
+    type_detection = State()
+    ml_foto = State()
 
 
 @dp.message(Command(commands=["start"]))
-async def command_handler(message: types.Message, state: FSMContext) -> None:
-    await message.answer("Это бот по детекции Дипфейков на фотографии! \
-                         \nХотите попробовать? \nВведите Да или Нет")
-    await state.set_state(States.state)
+async def command_handler(message: Message, state: FSMContext) -> None:
+    await message.answer(
+        text="Это бот по детекции Дипфейков на фотографии! \
+             \nХотите попробовать?",
+        reply_markup=generate_keyboard(["Да", "Нет"])
+    )
+    await state.set_state(States.yes_no)
 
 
-@dp.message(F.text.lower() == "да", States.state)
-async def continue_yes(message: types.Message,
+@dp.message(F.text.lower() == "да", States.yes_no)
+async def continue_yes(message: Message,
                        state: FSMContext) -> None:
-    await message.reply("Отличный выбор! \nКакой метод ML или DL.\
-                        \nНажмите Отмена для выхода")
-    await state.set_state(States.state_1)
+    await message.reply(
+        text="Отличный выбор! \nВыберите метод или нажмите Отмена для выхода",
+        reply_markup=generate_keyboard(["ML", "DL", "Отмена"], 3)
+    )
+    await state.set_state(States.type_detection)
 
 
-@dp.message(F.text.lower() == "нет", States.state)
-async def continue_no(message: types.Message,
+@dp.message(F.text.lower() == "нет", States.yes_no)
+async def continue_no(message: Message,
                       state: FSMContext) -> None:
-    await message.reply("Жаль! \nДо встречи в следующий раз")
+    await message.reply(
+        text="Жаль! \nДо встречи в следующий раз",
+        reply_markup=ReplyKeyboardRemove()
+    )
     await state.clear()
 
 
-@dp.message(States.state)
-async def continue_unexpected(message: types.Message) -> None:
-    await message.reply("Введите 'Да' для продолжения или 'Нет' для выхода")
+@dp.message(States.yes_no)
+async def continue_unexpected(message: Message) -> None:
+    await message.reply(
+        text="Введите 'Да' для продолжения или 'Нет' для выхода",
+        reply_markup=generate_keyboard(["Да", "Нет"])
+    )
 
 
-@dp.message(F.text.lower() == "ml", States.state_1)
-async def ml_method(message: types.Message,
+@dp.message(F.text.lower() == "ml", States.type_detection)
+async def ml_method(message: Message,
                     state: FSMContext) -> None:
-    await message.reply("Machine Learning - отличный выбор!\
-                        \nДобавьте фотографию")
-    await state.set_state(States.state_2)
+    await message.reply(
+        text="Machine Learning - отличный выбор!\nДобавьте фотографию",
+        reply_markup=ReplyKeyboardRemove()
+    )
+    await state.set_state(States.ml_foto)
 
 
-@dp.message(F.text.lower() == "dl", States.state_1)
-async def dl_method(message: types.Message) -> None:
-    await message.reply("Deep Learning. \
+@dp.message(F.text.lower() == "dl", States.type_detection)
+async def dl_method(message: Message) -> None:
+    await message.reply(
+        text="Deep Learning. \
             \nК сожалению, данный метод пока еще в разработке.\
-            \nВведите 'ML' для продолжения или 'Отмена' для выхода")
+            \nВведите 'ML' для продолжения или 'Отмена' для выхода",
+        reply_markup=generate_keyboard(["ML", "DL", "Отмена"], 3)
+    )
 
 
-@dp.message(States.state_1)
-async def unexpected_method(message: types.Message) -> None:
-    await message.reply("Введите ML или DL для продолжения \
-                        или 'Отмена' для выхода")
-
-
-@dp.message(F.text.lower() == "отмена", States.state_1)
-async def quit_method(message: types.Message, state: FSMContext) -> None:
-    await message.reply("Жаль! \nДо встречи в следующий раз")
+@dp.message(F.text.lower() == "отмена", States.type_detection)
+async def quit_method(message: Message, state: FSMContext) -> None:
+    await message.reply(
+        text="Жаль! \nДо встречи в следующий раз",
+        reply_markup=ReplyKeyboardRemove()
+    )
     await state.clear()
 
 
-@dp.message(F.photo, States.state_2)
+@dp.message(States.type_detection)
+async def unexpected_method(message: Message) -> None:
+    await message.reply(
+        text="Выберите метод или нажмите Отмена для выхода",
+        reply_markup=generate_keyboard(["ML", "DL", "Отмена"], 3)
+    )
+
+
+@dp.message(F.photo, States.ml_foto)
 async def ml_photo(message: Message, bot: Bot, state: FSMContext):
 
     await message.answer("Ваша фотография получена. \
--- test_bot.py
+++ test_bot.py
@@ -1,6 +1,7 @@
 import pytest
 from aiogram.filters import Command
 from aiogram.methods import SendMessage
+from aiogram.types import ReplyKeyboardMarkup
 
 from bot import command_handler
 from bot import continue_yes
@@ -10,11 +11,53 @@ from bot import ml_method
 from bot import dl_method
 from bot import unexpected_method
 from bot import quit_method
+from bot import generate_keyboard
 from bot import States
 
 from aiogram_tests import MockedBot
 from aiogram_tests.handler import MessageHandler
 from aiogram_tests.types.dataset import MESSAGE
+
+
+# Тест функции generate_keyboard
+def test_generate_keyboard():
+    # Создаем список кнопок
+    button_names = ["Да", "Нет", "Отмена"]
+
+    # количество кнопок в ряду
+    buttons_in_row = 3
+
+    # Вызываем функцию generate_keyboard
+    keyboard = generate_keyboard(button_names, buttons_in_row)
+
+    # Проверяем тип возвращаемого объекта
+    assert isinstance(keyboard, ReplyKeyboardMarkup)
+
+    # Проверяем, что количество кнопок в ряду не больше buttons_in_row
+    for row in keyboard.keyboard:
+        assert len(row) <= buttons_in_row
+
+    # Проверяем текст кнопок
+    expected_button_texts = ["Да", "Нет", "Отмена"]
+    for i, row in enumerate(keyboard.keyboard):
+        for j, button in enumerate(row):
+            assert button.text == expected_button_texts[i * 2 + j]
+
+
+def test_Responses():
+    # Создаем экземпляр класса States
+    responses = States()
+    expected_attr = ['yes_no', 'type_detection', 'ml_foto']
+
+    # Проверяем, что состояния имеют ожидаемые атрибуты
+    for attribute in expected_attr:
+        assert hasattr(responses, attribute)
+
+    # Проверяем, что каждое состояние уникально
+    for i, attribute in enumerate(expected_attr):
+        if len(expected_attr[i + 1:]) > 1:
+            for matched_attr in expected_attr[i + 1:]:
+                assert getattr(responses, attribute) != matched_attr
 
 
 @pytest.mark.asyncio
@@ -26,28 +69,27 @@ async def test_command_handler():
     calls = await requester.query(MESSAGE.as_object(text="/start"))
     answer_message = calls.send_message.fetchone().text
     assert answer_message == "Это бот по детекции Дипфейков на фотографии! \
-                         \nХотите попробовать? \nВведите Да или Нет"
+             \nХотите попробовать?"
 
 
 @pytest.mark.asyncio
 async def test_continue_yes():
     requester = MockedBot(
         request_handler=MessageHandler(continue_yes,
-                                       state=States.state))
+                                       state=States.yes_no))
     requester.add_result_for(SendMessage, ok=True)
     calls = await requester.query(MESSAGE.as_object(text="да"))
     answer_message = calls.send_message.fetchone().text
-    assert answer_message == "Отличный выбор! \nКакой метод ML или DL.\
-                        \nНажмите Отмена для выхода"
+    assert answer_message == "Отличный выбор! \nВыберите метод или нажмите Отмена для выхода"
 
 
 @pytest.mark.asyncio
 async def test_continue_no():
     requester = MockedBot(
         request_handler=MessageHandler(continue_no,
-                                       state=States.state))
+                                       state=States.yes_no))
     requester.add_result_for(SendMessage, ok=True)
-    calls = await requester.query(MESSAGE.as_object(text="отмена"))
+    calls = await requester.query(MESSAGE.as_object(text="нет"))
     answer_message = calls.send_message.fetchone().text
     assert answer_message == "Жаль! \nДо встречи в следующий раз"
 
@@ -56,7 +98,7 @@ async def test_continue_no():
 async def test_continue_unexpected():
     requester = MockedBot(
         request_handler=MessageHandler(continue_unexpected,
-                                       state=States.state))
+                                       state=States.yes_no))
     requester.add_result_for(SendMessage, ok=True)
     calls = await requester.query(MESSAGE.as_object())
     answer_message = calls.send_message.fetchone().text
@@ -65,19 +107,22 @@ async def test_continue_unexpected():
 
 @pytest.mark.asyncio
 async def test_ml_method():
-    requester = MockedBot(request_handler=MessageHandler(ml_method,
-                                                         state=States.state_1))
+    requester = MockedBot(
+        request_handler=MessageHandler(ml_method,
+                                       state=States.type_detection)
+    )
     requester.add_result_for(SendMessage, ok=True)
     calls = await requester.query(MESSAGE.as_object(text="ml"))
     answer_message = calls.send_message.fetchone().text
-    assert answer_message == "Machine Learning - отличный выбор!\
-                        \nДобавьте фотографию"
+    assert answer_message == "Machine Learning - отличный выбор!\nДобавьте фотографию"
 
 
 @pytest.mark.asyncio
 async def test_dl_method():
-    requester = MockedBot(request_handler=MessageHandler(dl_method,
-                                                         state=States.state_1))
+    requester = MockedBot(
+        request_handler=MessageHandler(dl_method,
+                                       state=States.type_detection)
+    )
     requester.add_result_for(SendMessage, ok=True)
     calls = await requester.query(MESSAGE.as_object(text="dl"))
     answer_message = calls.send_message.fetchone().text
@@ -88,19 +133,22 @@ async def test_dl_method():
 
 @pytest.mark.asyncio
 async def test_unexpected_method():
-    requester = MockedBot(request_handler=MessageHandler(unexpected_method,
-                                                         state=States.state_1))
+    requester = MockedBot(
+        request_handler=MessageHandler(unexpected_method,
+                                       state=States.type_detection)
+    )
     requester.add_result_for(SendMessage, ok=True)
     calls = await requester.query(MESSAGE.as_object())
     answer_message = calls.send_message.fetchone().text
-    assert answer_message == "Введите ML или DL для продолжения \
-                        или 'Отмена' для выхода"
+    assert answer_message == "Выберите метод или нажмите Отмена для выхода"
 
 
 @pytest.mark.asyncio
 async def test_quit_method():
-    requester = MockedBot(request_handler=MessageHandler(quit_method,
-                                                         state=States.state_1))
+    requester = MockedBot(
+        request_handler=MessageHandler(quit_method,
+                                       state=States.type_detection)
+    )
     requester.add_result_for(SendMessage, ok=True)
     calls = await requester.query(MESSAGE.as_object(text="Отмена"))
     answer_message = calls.send_message.fetchone().text
